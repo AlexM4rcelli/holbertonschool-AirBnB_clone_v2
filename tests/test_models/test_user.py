79e568fb87690736debb2855ec7db613ec243d26
acostanahuel <acostanahuel>
2023-11-22 16:55:11 -0300

    fix and update ex7

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""Defines the User class."""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 
+class User(BaseModel, Base):
+    """Represents a user for a MySQL database.
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
