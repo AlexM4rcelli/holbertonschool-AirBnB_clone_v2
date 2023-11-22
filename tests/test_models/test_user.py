7bc545796419e32a497a5219f47d7daa70ca1920
acostanahuel <acostanahuel>
2023-11-22 17:04:55 -0300

    update7

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,7 +2,8 @@
 """Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
-
+from sqlalchemy import Column
+from sqlalchemy import String
 
 
 class User(BaseModel, Base):
