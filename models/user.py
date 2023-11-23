e99be7f0e4561ad41c182503ad2eb1676308ecae
AlexM4rcelli <6780@holbertonstudents.com>
2023-11-23 18:38:47 +0000

    Feat: task 9

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines the User class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 
 class User(BaseModel, Base):
@@ -17,3 +17,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship('Place', cascade='all, delete-orphan',
+                               backref='user')
