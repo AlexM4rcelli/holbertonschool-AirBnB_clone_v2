f6be5dcebaa3f5a3f080950bcd97ed38bc050978
acostanahuel <acostanahuel17@gmail.com>
2023-11-25 10:45:51 -0300

    fix and test amenity

--- models/place.py
+++ models/place.py
@@ -25,11 +25,9 @@ association_table = Table("place_amenity", Base.metadata,
 
 
 class Place(BaseModel, Base):
-    """Represents a Place for a MySQL database.
+    """Represents a Place for a MySQL database."""
 
-    Inherits from SQLAlchemy Base and links to the MySQL table places.
 
-    """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
