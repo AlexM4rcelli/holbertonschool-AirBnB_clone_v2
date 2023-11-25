a3a41d63babe44c7f24cbd8895d53ba5215deeec
acostanahuel <acostanahuel17@gmail.com>
2023-11-25 10:34:10 -0300

    fix and agregate

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines the User class."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
@@ -17,6 +18,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
 
     places = relationship('Place', cascade='all, delete-orphan',
                                backref='user')
