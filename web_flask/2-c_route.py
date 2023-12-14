6fe4900b2a9dcec29bf48b9ff3f9e9e8b12f4f1a
AlexM4rcelli <6780@holbertonstudents.com>
2023-12-14 17:16:30 +0000

    Feat: Documentation

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hnbb():
+    """Displays 'HBNB'"""
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Displays 'C' followed by the value of <text>."""
     return f'C {text}'.replace('_', " ")
 
 if __name__ == '__main__':
